4687868bee39f3909816cd4aa9ca45e6d2b78f5f
Laptop <Laptop@flaptop.com>
2017-11-07 09:48:35 +0100

    Added wirtschaft sozial library

---
+++ WiSoLibrary/Exam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiSoLibrary
+{
+	public class Exam
+	{
+
+		/// <summary>
+		/// Parses the xml and returns the Exam with it's questions
+		/// </summary>
+		/// <param name="path">Path to the exams xml file</param>
+		/// <returns>The parsed exam</returns>
+		/// <exception cref="NotImplementedException"/>
+		public static Exam GetExamFromXML(string path)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ WiSoLibrary/Question.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiSoLibrary
+{
+    public struct Question
+    {
+		/// <summary>
+		/// Represents a question
+		/// </summary>
+		/// <param name="text">The actual question</param>
+		/// <param name="answers">List of answers that are accepted.</param>
+		public Question(string text, params Answer[] answers)
+		{
+			Text = text;
+			Answers = answers;
+		}
+
+		/// <summary>
+		/// The question Text
+		/// </summary>
+		public string Text { get; private set; }
+
+		/// <summary>
+		/// The possible Answers
+		/// </summary>
+		public Answer[] Answers { get; private set; }
+
+		/// <summary>
+		/// Filter on Answers where IsCorrect is true.
+		/// </summary>
+		public IEnumerable<Answer> CorrectAnswers { get { return Answers.Where(a => a.IsCorrect); } }
+	}
+}
