36af5097eba5c188b47f3819c9f8ebef5c4a511f
Noobl0r <nan@gmail.com>
2017-11-11 16:02:57 +0100

    added console programm and some basic logiuc

--- MultipleChoiceConsole/Program.cs
+++ MultipleChoiceConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WiSoLibrary;
 
 namespace MultipleChoiceConsole
 {
@@ -10,6 +11,30 @@ namespace MultipleChoiceConsole
 	{
 		static void Main(string[] args)
 		{
+			var d = Exam.GetExamFromXML(@"C:\Users\Mümmelmann\Google Drive\Alle WISO\Korrigierte\16_So_WISO\questions.xml");
+
+			foreach (var question in d.Questions.Randomize())
+			{
+				Console.WriteLine(question.Text);
+				Console.WriteLine();
+
+				var answers = question.Answers.Randomize();
+				for (int i = 0; i < answers.Length; i++)
+				{
+					Console.WriteLine($"\t{i + 1}. {answers[i].Text}");
+				}
+
+				Console.WriteLine();
+				Console.WriteLine();
+				Console.WriteLine("Type in the number of the correct answer.");
+
+				var cr = Console.ReadKey();
+				if (answers[int.Parse(cr.ToString()) - 1].IsCorrect)
+					Console.WriteLine("Correct");
+				else
+					Console.WriteLine("False");
+
+			}
 		}
 	}
 }
